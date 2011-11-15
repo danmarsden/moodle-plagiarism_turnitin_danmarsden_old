0dd9633148ea2f8d65a82bb5d7a0ac704dfb8852
Peter Bulmer <peter.bulmer@catalyst.net.nz>
2011-11-15 14:00:16 +1300

    event handling - don't include directories in list

--- lib.php
+++ lib.php
@@ -492,11 +492,8 @@ class plagiarism_plugin_turnitin extends plagiarism_plugin {
                     $submission = $assignmentbase->get_submission($eventdata->userid);
                     $modulecontext = get_context_instance(CONTEXT_MODULE, $eventdata->cmid);
                     $fs = get_file_storage();
-                    if ($files = $fs->get_area_files($modulecontext->id, 'mod_assignment', 'submission', $submission->id, "timemodified")) {
+                    if ($files = $fs->get_area_files($modulecontext->id, 'mod_assignment', 'submission', $submission->id, "timemodified", false)) {
                         foreach ($files as $file) {
-                            if ($file->get_filename()==='.') {
-                                continue;
-                            }
                             //TODO: need to check if this file has already been sent! - possible that the file was sent before draft submit was set.
                             $pid = plagiarism_update_record($cmid, $eventdata->userid, $file->get_pathnamehash());
                             if (!empty($pid)) {
