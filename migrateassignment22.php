97d632ae54dc0585d81cb4aaba3bfad85626be22
Dan Marsden <dan@danmarsden.com>
2012-12-14 21:57:24 +1300

    tidy up migration script.

--- migrateassignment22.php
+++ migrateassignment22.php
@@ -36,6 +36,7 @@ admin_externalpage_setup('plagiarismturnitin');
 $PAGE->set_url('/plagiarism/turnitin/migrateassignment22.php');
 
 echo $OUTPUT->header();
+echo $OUTPUT->motification("NASTY HACK ALERT - this script is a nasty hack that uses the logs table to translate old assignment ids to new 2.3 assignment ids - use at own risk!!!!");
 $currenttab='turnitinmigrate';
 require_once('turnitin_tabs.php');
 
@@ -47,10 +48,17 @@ if ($fix19assignments) { //script to fix 19 legacy assignments that weren't migr
     $tii_files = $DB->get_records('tii_files_legacy');
     if (!empty($tii_files)) {
         $i = 0;
+        $exists = 0;
+        $ignore = 0;
         foreach ($tii_files as $tf) {
             //first check if this file already exists in plagiarism_turnitin_files
+            if (empty($tf->tii)) {
+                $ignored++;
+                continue; //only migrate files that have a valid report.
+            }
             if ($DB->record_exists('plagiarism_turnitin_files', array('externalid' => $tf->tii))) {
-                echo "alreadyexists";
+                echo "alreadyexists - migrated previously:".$tf->tii."<br>";
+                $exists++;
                 continue;
             }
             $newf = new stdClass();
@@ -61,17 +69,26 @@ if ($fix19assignments) { //script to fix 19 legacy assignments that weren't migr
             $newf->similarityscore = $tf->tiiscore;
             $newf->legacyteacher = 1;
 
-            //now get oldcm based on course and module from log
-            $oldcm = $DB->get_field('log', 'cmid', array('course' => $tf->course, 'action' => 'delete mod', 'info' => 'assignment '.$tf->instance));
-            $newcm = $DB->get_field('log', 'cmid', array('course' => $tf->course, 'action' => 'view', 'module' => 'assign', 'info' => $oldcm));
-            if (!empty($oldcm) && !empty($newcm)) {
+
+            //$oldcm = $DB->get_field('log', 'cmid', array('course' => $tf->course, 'action' => 'delete mod', 'info' => 'assignment '.$tf->instance));
+            $newcm = $DB->get_field('log', 'cmid', array('course' => $tf->course, 'action' => 'view', 'module' => 'assign', 'info' => $tf->instance));
+            if (!empty($newcm)) {
                 $newf->cm = $newcm;
                 //now get the pathnamehash for this old file
                 //first get all the files from the assignment module.
                 $modulecontext = context_module::instance($newcm);
                 //get instanceid of this assign
                 $instanceid = $DB->get_field('course_modules', 'instance', array('id' => $newcm));
+                if (empty($instanceid)) {
+                    mtrace("couldn't find instanceid cm: ".$newcm);
+                    continue;
+                }
                 $submission = $DB->get_record('assign_submission', array('assignment'=>$instanceid, 'userid'=>$tf->userid));
+                if (empty($submission)) {
+                    mtrace("couldn't find submission: ".$instanceid. 'user:'.$tf->userid);
+                    continue;
+                }
+
                 $files = $fs->get_area_files($modulecontext->id, 'assignsubmission_file', 'submission_files', $submission->id, "id", false);
                 foreach ($files as $file) {
                     if ($file->get_filename()==$tf->filename) {
@@ -82,32 +99,42 @@ if ($fix19assignments) { //script to fix 19 legacy assignments that weren't migr
                     $i++;
                     $DB->insert_record('plagiarism_turnitin_files', $newf);
                 }
+            } else {
+                mtrace("could not find oldcm instance:".$tf->instance);
             }
         }
         echo "migrated ".$i . "files";
+        echo $exists . " already exist";
+        echo 'ignored' .$ignored . " files as they didn't have a valid report.";
     }
 }
 
 if ((data_submitted()) && confirm_sesskey()) {
-    $sql = "SELECT DISTINCT cmid, info FROM {log} WHERE module='assign' AND action='view'";
+    //first get instances of deleted mods
+    $sql = "SELECT DISTINCT cmid, info FROM {log} WHERE module='course' AND action='delete mod' AND info LIKE 'assignment%'";
     $rs = $DB->get_recordset_sql($sql);
     foreach ($rs as $r) {
-        if (is_numeric($r->info) && $r->cmid <> $r->info) {
-            $oldcm = (int) $r->info;
-            //update config with new cm
+        $instance = str_replace('assignment ', '', $r->info);
+        if (!is_numeric($instance)) {
+            mtrace("not valid instance:".$r->info);
+            continue;
+        }
+        //now get newcm based on instance.
+        //this is an old log entry that has the cmid translated but the old instanceid is kept in the info field.
+        $newcm = $DB->get_field('log', 'cmid', array('action' => 'view', 'module' => 'assign', 'info' => $instance));
 
-            if (!$DB->record_exists('course_modules', array('id' => $oldcm)) && //check that the $oldcm doesn't exist - if it still exists we shouldn't do anything here.
-                 $DB->record_exists('course_modules', array('id' => $r->cmid)) && //check that the new cm still exists.
-                 $DB->record_exists('plagiarism_turnitin_config', array('cm'=>$oldcm))) { //check that we have records to update.
+        if (!$DB->record_exists('course_modules', array('id' => $r->cmid)) && //sanity check that the $r->cmid doesn't exist - if it still exists we shouldn't do anything here.
+             $DB->record_exists('course_modules', array('id' => $newcm)) && //check that the new cm still exists.
+             $DB->record_exists('plagiarism_turnitin_config', array('cm'=>$r->cmid))) { //check that we have records to update.
 
-                //update turnitin config with new cm
-                $DB->set_field('plagiarism_turnitin_config', 'cm', $r->cmid, array('cm' => $oldcm));
+            //update turnitin config with new cm
+            $DB->set_field('plagiarism_turnitin_config', 'cm', $newcm, array('cm' => $r->cmid));
 
-                //update turnitin files with new cm
-                $DB->set_field('plagiarism_turnitin_files', 'cm', $r->cmid, array('cm' => $oldcm));
+            //update turnitin files with new cm
+            $DB->set_field('plagiarism_turnitin_files', 'cm', $newcm, array('cm' => $r->cmid));
 
             }
-        }
+
     }
     $rs->close();
     echo $OUTPUT->notification(get_string('migrated', 'plagiarism_turnitin'), 'notifysuccess');
